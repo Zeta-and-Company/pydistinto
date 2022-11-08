12531babbdb48ad6d5b9f8fb5d3debd78239e028
KeliDu <cndukeli@hotmail.com>
2022-11-08 23:04:18 +0800

    preprocess_spacy parser and ner disabled

--- scripts/pipeline/preprocess_spacy.py
+++ scripts/pipeline/preprocess_spacy.py
@@ -74,75 +74,75 @@ def main(plaintextfolder, taggedfolder, language, sanitycheck='no'):
     :param language
     """
     if language == "English":
-        nlp = spacy.load("en_core_web_sm")
+        nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # french models
     elif language == "French":
-        nlp = spacy.load("fr_core_news_sm")
+        nlp = spacy.load("fr_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Catalan models
     elif language == "Catalan":
-        nlp = spacy.load("ca_core_news_sm")
+        nlp = spacy.load("ca_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Chinese models
     elif language == "Chinese":
-        nlp = spacy.load("zh_core_web_sm")
+        nlp = spacy.load("zh_core_web_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Danish models
     elif language == "Danish":
-        nlp = spacy.load("da_core_news_sm")
+        nlp = spacy.load("da_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Dutch models
     elif language == "Dutch":
-        nlp = spacy.load("nl_core_news_sm")
+        nlp = spacy.load("nl_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # German models
     elif language == "German":
-        nlp = spacy.load("de_core_news_sm")
+        nlp = spacy.load("de_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Greek models
     elif language == "Greek":
-        nlp = spacy.load("el_core_news_sm")
+        nlp = spacy.load("el_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Italian models
     elif language == "Italian":
-        nlp = spacy.load("it_core_news_sm")
+        nlp = spacy.load("it_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Japanese models
     elif language == "Japanese":
-        nlp = spacy.load("ja_core_news_sm")
+        nlp = spacy.load("ja_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Lithuanian models
     elif language == "Lithuanian":
-        nlp = spacy.load("lt_core_news_sm")
+        nlp = spacy.load("lt_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Macedonian models
     elif language == "Macedonian":
-        nlp = spacy.load("mk_core_news_sm")
+        nlp = spacy.load("mk_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Norwegian Bokmål models
     elif language == "Norwegian Bokmål":
-        nlp = spacy.load("nb_core_news_sm")
+        nlp = spacy.load("nb_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Polish models
     elif language == "Polish":
-        nlp = spacy.load("pl_core_news_sm")
+        nlp = spacy.load("pl_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Portuguese models
     elif language == "Portuguese":
-        nlp = spacy.load("pt_core_news_sm")
+        nlp = spacy.load("pt_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Romanian models
     elif language == "Romanian":
-        nlp = spacy.load("ro_core_news_sm")
+        nlp = spacy.load("ro_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Russian models
     elif language == "Russian":
-        nlp = spacy.load("ru_core_news_sm")
+        nlp = spacy.load("ru_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     # Spanish models
     elif language == "Spanish":
-        nlp = spacy.load("es_core_news_sm")
+        nlp = spacy.load("es_core_news_sm", disable=['parser', 'ner'])
         nlp.max_length = 10000000
     else:
         raise ValueError("Unable to load spacy model. Please check your language settings.")
